eb07889db18d817b61d871731d2c72273007a311
melon <ortizfranco48@gmail.com>
2023-03-08 19:57:14 -0300

    grapping app inside AuthContextProviderr

--- google-auth/src/App.js
+++ google-auth/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import {AuthContextProvider} from './context/AuthContext';
 import Account from './pages/Account';
 import Signin from './pages/Signin';
 import Home from './pages/Home';
