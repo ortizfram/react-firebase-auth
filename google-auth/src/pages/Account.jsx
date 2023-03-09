aa0d89acfcc54c1392bdf83a9dce5dd1a1445314
ortizfram <ortizfranco48@gmail.com>
2023-03-09 09:40:49 -0300

    welcome userName

--- google-auth/src/pages/Account.jsx
+++ google-auth/src/pages/Account.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import { UserAuth, user } from "../context/AuthContext";
 
 const Account = () => {
+  const { logOut, user } = UserAuth();
+
+  const handleSignOut = async () => {
+    try {
+      await logOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-      <h1 className='text-bold text-3xl text-center'>Account</h1>
+      <h1 className="text-bold text-3xl text-center">Account</h1>
+      <p>Welcome, {user?.displayName}!!</p>
+      <div>
+        <button onClick={handleSignOut} className="border py-2 px-5 mt-10">
+          Logout
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Account
+export default Account;
