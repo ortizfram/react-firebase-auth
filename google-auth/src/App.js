ad730d50a2d22a379791cb2d17075ed2d2eb2a62
ortizfram <ortizfranco48@gmail.com>
2023-03-09 09:47:02 -0300

    redirecting to home when is not logged in

--- google-auth/src/App.js
+++ google-auth/src/App.js
@@ -5,6 +5,7 @@ import {AuthContextProvider} from './context/AuthContext';
 import Account from './pages/Account';
 import Signin from './pages/Signin';
 import Home from './pages/Home';
+import Protected from './components/Protected';
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signin" element={<Signin />} />
-          <Route path="/account" element={<Account />} />
+          <Route path="/account" element={<Protected><Account /></Protected>} />
         </Routes>
       </AuthContextProvider>
     </div>
