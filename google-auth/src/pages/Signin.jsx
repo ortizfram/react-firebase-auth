644669b56de77826d6d22b081d782cdfe6e5ae43
ortizfram <ortizfranco48@gmail.com>
2023-03-09 08:49:24 -0300

    handle signin google button

--- google-auth/src/pages/Signin.jsx
+++ google-auth/src/pages/Signin.jsx
@@ -1,24 +1,23 @@
 import React from "react";
 import GoogleButton from "react-google-button";
-import { userAuth } from "../context/AuthContext";
+import { UserAuth } from "../context/AuthContext";
 
 const Signin = () => {
-  //const { googleSignIn } = UserAuth();
+  const { googleSignIn } = UserAuth();
 
-  // const handleGoogleSignIn = async () => {
-  //   try {
-  //     await googleSignIn();
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+  const handleGoogleSignIn = async () => {
+    try {
+      await googleSignIn();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
       <h1 className="text-center text-3xl font-bold py-8">Sign in</h1>
       <div className="max-w-[240px] m-auto py-4">
-        <GoogleButton />
-        {/* <GoogleButton onClick={handleGoogleSignIn} />*/}
+        <GoogleButton onClick={handleGoogleSignIn} />
       </div>
     </div>
   );
