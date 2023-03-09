eb1ac4f910a6a115c7e0edf979feae68de570c5d
ortizfram <ortizfranco48@gmail.com>
2023-03-09 09:12:26 -0300

    already signed in

--- google-auth/src/context/AuthContext.js
+++ google-auth/src/context/AuthContext.js
@@ -1,23 +1,34 @@
-import {useContext, createContext} from 'react';
+import {useContext, createContext, useEffect, useState} from 'react';
 import { GoogleAuthProvider, signInWithPopup, signInWithRedirect, signOut, onAuthStateChanged } from "firebase/auth";
 import { auth } from '../firebase';
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 
 export const AuthContextProvider = ({children}) => {
+    const [user, setUser] = useState({});
 
     const googleSignIn = () => {
         const provider = new GoogleAuthProvider();
         signInWithPopup(auth, provider)
-    }
+    };
+
+    useEffect(()=> {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            console.log('User', currentUser)
+        });
+        return () => {
+            unsubscribe();
+        };
+    },[]);
 
     return (
         <AuthContext.Provider value={{googleSignIn}}>
             {children}
         </AuthContext.Provider>
-    )
-}
+    );
+};
 
 export const UserAuth = () => {
-    return useContext(AuthContext)
-}
+    return useContext(AuthContext);
+};
